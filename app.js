c33878bc57b86fae9aa9ef1ba6f784f6abd8f0e8
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-05-10 08:04:57 +0700

    big change

--- app.js
+++ app.js
@@ -1,191 +1,37 @@
 gsap.registerPlugin(ScrollTrigger);
-
 // SECTION 1-2
 let sections = gsap.utils.toArray(".panel");
 
 const scrollhorizontal = gsap.timeline();
 
-scrollhorizontal.to("#section1-2 .mask4",1, {
-  scaleY: 0,
-  transformOrigin: "top bottom"
-},0).to("#section1-2 .orang",1, {
-  scale:1,
-  duration:3,
-},0).from("#section1-2 .textanim",1, {
-  opacity:0,
-  x:"7vh",
-},0).from("#section1-2 .qualification",1, {
-  opacity:0,
-  x:"-10vh",
-  stagger:0.1
-},0).to("#shadowsection .scrollbar",1, { 
-  width:"5%"
-},0).to("#shadowsection .sfase5",1, { 
-  scale:1,
-},0);
-
-scrollhorizontal.from("#section1-2 .scrollToExplore",1, {
-  opacity:0,
-  duration: 1,
-},0);
-
-scrollhorizontal.to("#section1-2 .quaimg",1, {
-  scale:0
-},1).to("#section1-2 .textanim",1, {
-  opacity:0,
-  x:"7vh",
-},1).from("#section1-2 .quacheck",1, {
-  scale:0
-},1).from("#section1-2 .textanim2",0.5, {
-  opacity:0,
-  x:"7vh",
-  delay:0.5,
-},1);
-
-scrollhorizontal;
 // SECTION1-2
 scrollhorizontal.to(sections,1, {
+  xPercent: -0
+},0);
+scrollhorizontal.to(sections,1, {
   xPercent: -100
-},2).to("#shadowsection .sfase5",1, { 
-  scale:0,
-},2).to("#shadowsection .sfase4",1, { 
-  scale:1,
-},2).to("#shadowsection .scrollbar",1, { 
-  width:"25%"
+},1);
+// SECTION1-3
+
+// SECTION1-3
+scrollhorizontal.to(sections,1, {
+  xPercent: -100
 },2);
-// SECTION1-3
-scrollhorizontal.to("#section1-3 .mask4",1, {
-  scaleY: 0,
-  transformOrigin: "top bottom",
-  duration: 1,
-},3).from("#section1-3 .orang",1, {
-  scale: 0.85,
-  transformOrigin: "center bottom",
-  duration: 1,
-},3).from("#section1-3 .textanim",1, {
-  opacity:0,
-  x:"3vh",
-},3).to("#shadowsection .scrollbar",1, { 
-  width:"30%"
-},3);
-
-scrollhorizontal.from("#section1-3 .animkanan",1, {
-  opacity:0,
-  x:"-10vh",
-  stagger:0.1,
-  duration:1,
-},4).from("#section1-3 .animkiri",1, {
-  opacity:0,
-  x:"10vh",
-  duration:1,
-},4).from("#section1-3 .scrollToExplore",1, {
-  opacity:0,
-  duration: 1,
-},4);
-
-scrollhorizontal.to("#section1-3 .animkanan",1, {
-  opacity:0,
-  x:"-10vh",
-  stagger:0.1,
-  duration:1,
-},5).to("#section1-3 .animkiri",1, {
-  opacity:0,
-  x:"10vh",
-  duration:1,
-},5).to("#section1-3 .textanim",1, {
-  opacity:0,
-  x:"3vh",
-},5);
-
-scrollhorizontal.from("#section1-3 .animkanan_after",1, {
-  opacity:0,
-  x:"-13vh",
-  stagger:0.1,
-  duration:1,
-},6).from("#section1-3 .animkiri_after",1, {
-  opacity:0,
-  x:"10vh",
-  duration:1,
-  stagger:0.1,
-},6).to("#section1-3 .scrollToExplore",1, {
-  opacity:1,
-  duration: 1,
-},6);
-// SECTION1-3
 scrollhorizontal.to(sections,1, {
   xPercent: -200
-},7).to("#shadowsection .sfase4",1, { 
-  scale:0,
-},7).to("#shadowsection .sfase3",1, { 
-  scale:1,
-},7).to("#shadowsection .scrollbar",1, { 
-  width:"50%"
-},7);
+},3);
+scrollhorizontal.to(sections,1, {
+  xPercent: -200
+},4);
 // SECTION1-4
-scrollhorizontal.to("#section1-4 .changetext5", 1, {
-  text: {
-    value: "Oops, it appears that this schedule is already full",
-  },
-  yoyo:1,
-  repeat:1,
-},8);
 
-scrollhorizontal.to("#section1-4 .kalender", 1, { 
-  x: "-100vh" 
-},9);
-
-scrollhorizontal.to("#section1-4 .changetext5", 1, {
-  text: { value: "Don't worry, EVA's here to help", ease: Linear.easeNone },
-},10).to("#section1-4 .instruction", 1, { 
-  opacity: 1 
-},10);
-// SECTION1-4
 scrollhorizontal.to(sections,1, {
   xPercent: -300
-},11).to("#shadowsection .sfase3",1, { 
-  scale:0,
-},11).to("#shadowsection .sfase2",1, { 
-  scale:1,
-},11).to("#shadowsection .scrollbar",1, { 
-  width:"75%"
-},11);
+},5);
+scrollhorizontal.to(sections,1, {
+  xPercent: -300
+},6);
 // SECTION2-1
-scrollhorizontal.from("#section2-1 .gambarsurat", 1, {
-  x:"100vh",
-  rotation:-13,
-},12).from("#section2-1 .yellowline", 1, {
-  scaleX:0,
-},12);
-
-scrollhorizontal.from("#section2-1 .notif", 1, {
-  scale:0,
-  transformOrigin:"top right",
-},13);
-
-scrollhorizontal.to("#section2-1 .notif", 1, {
-  scale:0,
-  transformOrigin:"top right",
-},14);
-
-scrollhorizontal.from("#section2-1 .foto-surat1", 1, {
-  scale:0,
-  transformOrigin:"top left",
-},15);
-
-scrollhorizontal.to("#section2-1 .foto-surat1", 1, {
-  x:130,
-  y:-100,
-  rotation:10,
-},16).from("#section2-1 .foto-surat2", 1, {
-  scale:0,
-  transformOrigin:"top left",
-},16).to("#shadowsection .scrollbar",1, { 
-  width:"100%"
-},16).to("#shadowsection .sfase2",1, { 
-  scale:0,
-},16).to("#shadowsection .sfase1",1, { 
-  scale:1,
-},16);
 
 let jingle = gsap.to('.notif',3,{rotation:-10,repeat: -1,yoyo:true});
 // SECTION2-1
@@ -194,8 +40,204 @@ ScrollTrigger.create({
   trigger: ".ccontainer",
   pin: true,
   scrub: 1,
-  snap: 1/17,
-  end: () => "+=10000" 
+  end: () => "+=4000" 
+});
+
+const sec_1 = gsap.timeline();
+
+sec_1.to("#section1-2 .mask4",1, {
+  scaleY: 0,
+  transformOrigin: "top bottom"
+},0).to("#section1-2 .orang",1, {
+  scale:1,
+  duration:3,
+},0).from("#section1-2 .textanim",1, {
+  opacity:0,
+  x:"7vh",
+  lazy: false,
+},0).from("#section1-2 .qualification",1, {
+  opacity:0,
+  x:"-10vh",
+  stagger:0.1,
+  lazy: false,
+},0).from("#section1-2 .scrollToExplore",1, {
+  opacity:0,
+  duration: 1,
+},0);
+
+ScrollTrigger.create({
+  animation: sec_1,
+  trigger: ".ccontainer",
+  markers:true,
+  start: "20% center",
+  end: "20% center",
+});
+
+const sec_1_2 = gsap.timeline();
+
+sec_1_2.to("#section1-2 .quaimg",1, {
+  scale:0
+},0).fromTo("#section1-2 .textanim",1, {
+  x:"0",
+},{
+  opacity:0,
+  x:"7vh",
+},0);
+
+ScrollTrigger.create({
+  animation: sec_1_2,
+  trigger: "#section1-2",
+  markers:true,
+  start: "70% center",
+  end: "70% center",
+});
+
+const sec_1_3 = gsap.timeline();
+
+sec_1_3.from("#section1-2 .quacheck",1, {
+  scale:0
+},0).from("#section1-2 .textanim2",1, {
+  opacity:0,
+  x:"7vh",
+},0);
+
+ScrollTrigger.create({
+  animation: sec_1_3,
+  trigger: "#section1-2",
+  markers:true,
+  start: "80% center",
+  end: "80% center",
+});
+
+const sec_2 = gsap.timeline();
+
+sec_2.to("#section1-3 .mask4",1, {
+  scaleY: 0,
+  transformOrigin: "top bottom",
+  duration: 1,
+},0).from("#section1-3 .orang",1, {
+  scale: 0.85,
+  transformOrigin: "center bottom",
+  duration: 1,
+},0).from("#section1-3 .textanim",1, {
+  opacity:0,
+  x:"3vh",
+},0).fromTo("#section1-3 .animkanan",1, {
+  opacity:0,
+  x:"-10vh",
+  duration:1,
+},{
+  opacity:1,
+  x:"0",
+  stagger:0.1,
+  duration:1,
+},0).from("#section1-3 .animkiri",1, {
+  opacity:0,
+  x:"10vh",
+  duration:1,
+},0).from("#section1-3 .scrollToExplore",1, {
+  opacity:0,
+  duration: 1,
+},0);
+
+ScrollTrigger.create({
+  animation: sec_2,
+  trigger: "#section1-2",
+  markers:true,
+  start: "170% center",
+  end: "170% center",
+});
+
+const sec_2_2 = gsap.timeline();
+
+sec_2_2.fromTo("#section1-3 .animkanan",1, {
+  x:"0vh",
+},{
+  opacity:0,
+  x:"-13vh",
+},0).fromTo("#section1-3 .animkiri",1, {
+  x:"0",
+},{
+  opacity:0,
+  x:"10vh",
+},0).fromTo("#section1-3 .textanim",1, {
+  x:"0",
+},{
+  opacity:0,
+  x:"3vh",
+},0);
+ScrollTrigger.create({
+  animation: sec_2_2,
+  trigger: "#section1-2",
+  markers:true,
+  start: "215% center",
+  end: "215% center",
+});
+
+const sec_2_3 = gsap.timeline();
+
+sec_2_3.from("#section1-3 .animkanan_after",1, {
+  opacity:0,
+  x:"-13vh",
+  lazy: false,
+},0).from("#section1-3 .animkiri_after",1, {
+  opacity:0,
+  x:"10vh",
+  lazy: false,
+},0);
+ScrollTrigger.create({
+  animation: sec_2_3,
+  trigger: "#section1-2",
+  markers:true,
+  start: "235% center",
+  end: "235% center",
+});
+
+const sec_3 = gsap.timeline();
+
+const reverseTween = gsap.to("#section1-4 .changetext5", {
+  text: {
+    value: "Ups,ternyata jadwal interview saat ini sudah terisi penuh",
+  },
+  ease:"none"
+}).reverse(0);
+
+sec_3.to("#section1-4 .changetext5", {
+  text: {
+    value: "Ups,ternyata jadwal interview saat ini sudah terisi penuh",
+  },
+  ease:"none"
+});
+sec_3.add(reverseTween);
+sec_3.to("#section1-4 .changetext5", {
+  text: {
+    value: "",
+  },
+  ease:"none"
+});
+ScrollTrigger.create({
+  animation: sec_3,
+  trigger: "#section1-2",
+  markers:true,
+  start: "300% center",
+  end: "300% center",
+});
+
+const sec_3_2 = gsap.timeline();
+sec_3_2.to("#section1-4 .kalender",1, { 
+  x: "-100vh" 
+},1).to("#section1-4 .changetext5", 1, {
+  text: { value: "Jangan khawatir, EVA ada untuk membantu", ease: Linear.easeNone },
+},1).to("#section1-4 .instruction", 1, { 
+  opacity: 1 
+},1);
+
+ScrollTrigger.create({
+  animation: sec_3_2,
+  trigger: "#section1-2",
+  markers:true,
+  start: "340% center",
+  end: "340% center",
 });
 
 var $toggle = 0;
@@ -233,5 +275,3 @@ $("#section1-4 .x").click(function () {
 });
 let cursortext = gsap.to('.cursortext',{opacity:0,ease:"power2.inOut",repeat: -1});
 // SECTION 1-4
-
-
